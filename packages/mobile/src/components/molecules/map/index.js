3e980549199771532f10924391e6a8c148cf8f0a
Gabriel Sozza Taveira José <gabrielstaveira@gmail.com>
2019-10-23 11:19:18 -0300

    Fixed permission message

--- packages/mobile/src/components/molecules/map/index.js
+++ packages/mobile/src/components/molecules/map/index.js
@@ -17,15 +17,6 @@ function Map({markers = [], onMarkerPress, ...props}) {
     const GetPos = async () => {
       const granted = await PermissionsAndroid.request(
         PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
-        {
-          title: 'Cool Photo App Camera Permission',
-          message:
-            'Cool Photo App needs access to your camera ' +
-            'so you can take awesome pictures.',
-          buttonNeutral: 'Ask Me Later',
-          buttonNegative: 'Cancel',
-          buttonPositive: 'OK',
-        },
       );
       if (granted === PermissionsAndroid.RESULTS.GRANTED) {
         const watchId = Geolocation.watchPosition(
